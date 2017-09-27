9b7c7f2fa5df28eb034db340563d5638bc4f8279
Yastrenky <ybramos2014@gmail.com>
2017-09-26 21:28:12 -0400

    yastrenky  commit

--- closures.js
+++ closures.js
@@ -6,8 +6,8 @@ Challenge 1
 // // now we'll call the function we just created
 // function1(); //should console.log('hello');
 
-function createFunction(){
-  function function1(){
+function createFunction() {
+  function function1() {
     console.log("hello");
   }
   return function1;
@@ -24,8 +24,8 @@ Challenge 2
 // printSample(); //should console.log('sample');
 // printHello(); //should console.log('hello');
 
-function createFunctionPrinter(str){
-  function result(){
+function createFunctionPrinter(str) {
+  function result() {
     console.log(str);
   }
   return result;
@@ -50,14 +50,17 @@ Challenge 3
 
 function outer() {
   var counter = 0;
-  function incrementCounter () {
-    counter ++;
+
+  function incrementCounter() {
+    counter++;
     console.log('counter', counter);
   }
   return incrementCounter;
 }
-var willCounter = outer(); counter 1
-var jasCounter = outer(); counter 2
+var willCounter = outer();
+counter 1
+var jasCounter = outer();
+counter 2
 
 Challenge 4
 // Now we are going to create a function addByX that returns a function that will add an input by x.
@@ -74,23 +77,28 @@ Challenge 4
 // addByFour(4); //should return 8
 // addByFour(10); //should return 14
 
-function addByX(num){
+function addByX(num) {
 
-  function add(yas){
-   return yas + num;
+  function add(yas) {
+    return yas + num;
   }
   return add;
 }
 
 Extension: Challenge 5
-Write a function once that accepts a callback as input and returns a function. When the returned function is called the first time,
-it should call the callback and return that output. If it is called any additional times,
-instead of calling the callback again it will simply return the output value from the first time it was called.
+Write a
+function once that accepts a callback as input and returns a
+function.When the returned
+function is called the first time,
+it should call the callback and
+return that output.If it is called any additional times,
+  instead of calling the callback again it will simply
+return the output value from the first time it was called.
 
-function oneFunction(func){
+function oneFunction(func) {
   var result;
-  return function(){
-    if(func){
+  return function() {
+    if (func) {
       result = func.apply(null, arguments);
       func = null;
     }
@@ -99,22 +107,26 @@ function oneFunction(func){
 }
 
 Extension: Challenge 6
-Write a function after that takes the number of times the callback needs to be called before being
+Write a
+function after that takes the number of times the callback needs to be called before being
 executed as the first parameter and the callback as the second parameter.
 
- function  after(num, func){
-   var counter = 1;
-   function incrementCounter () {
-if(counter==num) {func()}
-counter ++;
-   }
-   return incrementCounter;
- }
-var call = after(5,function(){
-   console.log("win");
- });
+function after(num, func) {
+  var counter = 1;
 
- call();
+  function incrementCounter() {
+    if (counter == num) {
+      func.apply(null, arguments)
+    }
+    counter++;
+  }
+  return incrementCounter;
+}
+var call = after(5, function() {
+  console.log("win");
+});
+
+call();
 
 
 Extension: Challenge 7
@@ -125,9 +137,10 @@ Extension: Challenge 7
 //  HINT: research setTimeout();
 
 
-function delay(func,num){
-return setTimeout(func,num);
+function delay(func, num) {
+  //var args = Array.prototype.slice.call(arguments,2)
+  return setTimeout(func, num, arguments);
 }
-delay(function(){
-   console.log("win");
- },1000);
+delay(function() {
+  console.log("win");
+}, 1000);
